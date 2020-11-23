5f2685f0347c126a6a0671302b5ca1beecbb3de9
yoonthegoon <mby6mk+dqn1bcda2lwdykk9by4m0hhb7w@grr.la>
2020-11-23 12:42:52 -0500

    Update gmaps.py

--- bin/gmaps.py
+++ bin/gmaps.py
@@ -33,4 +33,4 @@ def locate(key: str = os.environ.get('API key'), address: str = None):
 
 
 if __name__ == '__main__':
-    print(locate(address='new york new york'))
+    print(locate(address='Manhattan'))
