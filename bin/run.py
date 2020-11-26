1f4a41f31ba0f19bab09ca2cb95ff03efde156f7
yoonthegoon <mby6mk+dqn1bcda2lwdykk9by4m0hhb7w@grr.la>
2020-11-26 14:55:15 -0500

    Update run.py

--- bin/run.py
+++ bin/run.py
@@ -5,19 +5,12 @@ import pandas as pd
 
 
 def main(address):
-    region = classes.Region('US', 'New Jersey', None, None, 'Paterson')  # functions.locate(address=address)
+    region = functions.locate(address=address)
 
     if region.aal1 and region.cc in countries.countries:
         if region.cc == 'US':
             data = countries.us.run(region)
             return data
     else:
-        print('you gotta do WHO')
-
-
-if __name__ == '__main__':
-    print(
-        main(
-            address='paterson nj'
-        )
-    )
+        # TODO: write who.py
+        return 0
